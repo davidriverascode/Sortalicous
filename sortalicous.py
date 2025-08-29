9697654777ea2bfb8339c306615fd596a806eed7
David Rivera <davidsrivera0@gmail.com>
2025-08-28 23:18:04 -0400

    Outline

--- sortalicous.py
+++ sortalicous.py
@@ -1,4 +1,30 @@
 import os
 import shutil
 
-print("Test")
+def line():
+    print("-----------------------------------")
+    
+def surround(string, input=False):
+
+    if input:
+        line()
+        new_string = input(string)
+        line()
+        return new_string
+
+    line()
+    print(string)
+    line()
+
+
+
+
+
+
+#ANCHOR - Outline
+# Sort images
+# Sort documents based off of content?
+# Run on startup
+# Allow user to create custom sorts
+# Sort games?
+# Sort Downloads folder -> installers
